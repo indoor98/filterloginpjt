ffa38c958525dfa63c1190745de02be56bef24ca
jmh <thinghoon@icloud.com>
2024-05-08 16:32:22 +0900

    Initial commit

---
+++ src/main/java/com/th/filterloginpjt/FilterloginpjtApplication.java
@@ -0,0 +1,13 @@
+package com.th.filterloginpjt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilterloginpjtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FilterloginpjtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/th/filterloginpjt/FilterloginpjtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.th.filterloginpjt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilterloginpjtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
