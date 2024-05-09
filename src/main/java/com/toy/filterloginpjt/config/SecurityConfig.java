df2e92583c79f4f8223c7d169992308d9ad2b65e
jmh <thinghoon@icloud.com>
2024-05-09 10:39:34 +0900

    H2 Inmemory DB 설정

---
+++ src/main/java/com/toy/filterloginpjt/config/SecurityConfig.java
@@ -0,0 +1,47 @@
+package com.toy.filterloginpjt.config;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+        http.sessionManagement((session) -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .csrf((csrf) -> csrf.disable())
+                .cors(corsCustomizer -> corsCustomizer.configurationSource(new CorsConfigurationSource() {
+                    @Override
+                    public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
+                        CorsConfiguration config = new CorsConfiguration();
+                        config.setAllowedOrigins(Collections.singletonList("*"));
+                        config.setAllowedMethods(Collections.singletonList("*"));
+                        config.setAllowCredentials(true);
+                        config.setAllowedHeaders(Collections.singletonList("*"));
+                        config.setExposedHeaders(Arrays.asList("Authorization"));
+                        config.setMaxAge(3600L);
+                        return config;
+                    }
+                }))
+                .authorizeHttpRequests((requests) -> {
+                    requests.requestMatchers(
+                            "/api/v1/auth/signup",
+                            "/api/v1/auth/signin",
+                            "/h2-console/**").permitAll()
+                            .anyRequest().authenticated();
+                });
+
+        http.headers(options -> options.frameOptions(frame -> frame.disable()));
+        http.formLogin( conf -> conf.disable() );
+        return (SecurityFilterChain)http.build();
+    }
+}
